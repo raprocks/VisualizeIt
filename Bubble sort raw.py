d2e225f96d1ba0af0e57258f611386cffc2c53f2
akhatavkar17 <75712866+akhatavkar17@users.noreply.github.com>
2020-12-09 18:36:09 +0530

    Add files via upload

---
+++ Bubble sort raw.py
@@ -0,0 +1,21 @@
+class BubbleSort :
+        def bubblesort(arr, n):
+            arr = []*100
+            n = int(input("Enter number of elements : "))
+            temp = 0
+            print("Enter the elements")
+            for k in range(0, self.n):
+                ele = list(input())
+                arr.append(ele) # adding the element
+
+            for i in range (0,n):
+                 for j in range (1,n-1):
+                          if arr[j-1] > arr[j] :
+                                 #swap elements
+                                 temp = self.arr[j-1]
+                                 arr[j-1] = self.arr[j]
+                                 arr[j] = self.temp
+
+            print ("The sorted array is = ")
+            for i in range (0, len(arr)):
+                print (arr[i])
