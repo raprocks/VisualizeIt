a1d079316ec2a4882bb515c97779fdf4ccb4948a
raprocks <rahulhimesh09@gmail.com>
2020-12-10 12:23:53 +0530

    context construction

--- VisualizeIt_implementations/BinarySearch.py
+++ VisualizeIt_implementations/BinarySearch.py
@@ -4,8 +4,17 @@ from typing import Union
 class BinarySearch:
     def __init__(self, arr: Union[list, tuple]):
         self.arr = arr
+        self.data = {
+            'algorithm': {
+                'type': 'search',
+                'name': 'Binary Search',
+            },
+            'array': self.arr
+        }
+        return self.data
 
     def step_search(self, element: int):
+        self.data['found'] = False
         start, end = 0, len(self.arr)-1
         mid = (start + end) // 2
         while start <= end:
@@ -14,9 +23,12 @@ class BinarySearch:
             elif self.arr[mid] > element:
                 end = mid - 1
             elif self.arr[mid] == element:
-                return mid
+                self.data['found'] = True
+                self.data[]
+                yield self.data
         if start > end:
-            return -1
+            self.data['found'] = False
+            return self.data
 
     def binarySearch(self, arr, first, last, key):
         mid = (first + last) / 2
