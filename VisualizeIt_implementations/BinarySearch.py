53f1c8a2c3eae4d3d1fdb4a0ae417058f726affc
raprocks <rahulhimesh09@gmail.com>
2020-12-13 12:07:33 +0530

    Indentation Fixed

--- VisualizeIt_implementations/BinarySearch.py
+++ VisualizeIt_implementations/BinarySearch.py
@@ -23,38 +23,37 @@ class BinarySearch:
         self.data['process_data']['mid'] = mid
         self.data['searching'] = True
         yield self.data
-        self.data['msg'] = f"Current Start "
-          while start <= end:
-
-               if self.arr[mid] < element:
-                    self.data['comparing'] = True
-                    self.data['comparing_data'] = {
-                        'ele1': self.arr[mid],
-                        'ele2': element
-                    }
-                    start = mid+1
-                    self.data['msg'] = f"Setting 'start' to {start}"
-                    self.data['process_data']['start'] = start
-                    yield self.data
-
-                elif self.arr[mid] > element:
-                    self.data['comparing'] = True
-                    self.data['comparing_data'] = {
-                        'ele1': self.arr[mid],
-                        'ele2': element}
-                    end = mid-1
-                    self.data['msg'] = f"Setting 'end' to {end}"
-                    self.data['process_data']['end'] = end
-                    yield self.data
-                elif self.arr[mid] == element:
-                    self.data['found'] = True
-                    self.data['searching'] = False
-                    self.data['msg'] = f"{element} Found at Index {mid}"
-                    return self.data
-                mid = (start+end)//2
-                self.data['process_data']['mid'] = mid
-                self.data['msg'] = f"Setting 'mid' to {mid}"
+        self.data['msg'] = f"Current Start is {start}, Mid is {mid} and End is {end}"
+        while start <= end:
+            if self.arr[mid] < element:
+                self.data['comparing'] = True
+                self.data['comparing_data'] = {
+                    'ele1': self.arr[mid],
+                    'ele2': element
+                }
+                start = mid+1
+                self.data['msg'] = f"Setting 'start' to {start}"
+                self.data['process_data']['start'] = start
                 yield self.data
+
+            elif self.arr[mid] > element:
+                self.data['comparing'] = True
+                self.data['comparing_data'] = {
+                    'ele1': self.arr[mid],
+                    'ele2': element}
+                end = mid-1
+                self.data['msg'] = f"Setting 'end' to {end}"
+                self.data['process_data']['end'] = end
+                yield self.data
+            elif self.arr[mid] == element:
+                self.data['found'] = True
+                self.data['searching'] = False
+                self.data['msg'] = f"{element} Found at Index {mid}"
+                return self.data
+            mid = (start+end)//2
+            self.data['process_data']['mid'] = mid
+            self.data['msg'] = f"Setting 'mid' to {mid}"
+            yield self.data
         if start > end:
             self.data['searching'] = False
             self.data['found'] = False
