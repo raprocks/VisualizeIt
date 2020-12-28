a6923f7a96fd1fd1933b0284ae71dc283ddcec1b
Rohit Patil <rahulhimesh09@gmail.com>
2020-12-28 17:38:29 +0530

    setup.py modified

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('./README.md') as fd:
+    long_des = fd.read()
+
+setup(
+    name="VisualizeIt",
+    version="0.1",
+    description="Terminal based Algorithm Visualizer made in Python3 with Typer and Colorama",
+    author="Rohit Patil",
+    packages=['VisualizeIt'],
+    author_email="rahulhimesh09@gmail.com",
+    license="GPLv3+",
+    url="https://github.com/raprocks/VisualizeIt",
+    install_requires=["colorama", "typer"], long_description=long_des,
+    long_description_content_type="text/markdown",
+    package_dir={'VisualizeIt': "VisualizeIt", },
+    package_data={
+        "VisualizeIt": ["VisualizeIt_implementations/*"]
+    },
+    entry_points={
+        "console_scripts": ["visualize=VisualizeIt.main:app"
+                            ]
+    }
+)
