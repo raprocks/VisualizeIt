65757054fb5198b53e5907bd1dbdca98b816911e
Rohit Patil <rahulhimesh09@gmail.com>
2021-02-26 18:21:06 +0530

    idk

--- VisualizeIt/visualizer.py
+++ VisualizeIt/visualizer.py
@@ -4,6 +4,7 @@ import sys
 from colorama import init, Fore, Style, deinit, Cursor
 from VisualizeIt_implementations.BinarySearch import BinarySearch
 from VisualizeIt_implementations.BubbleSort import BubbleSort
+from .VisualizeIt_implementations import ImprovedBubbleSort
 init()
 
 POS = Cursor.POS()
@@ -164,3 +165,80 @@ Style.RESET_ALL}
             except StopIteration:
                 pass
         deinit()
+
+
+
+class ImprovedSortVisualizer(Visualizer):s
+    def __init__(self, algoimpl: BubbleSort) -> None:
+        super().__init__(algoimpl)
+        self.algo = algoimpl
+        self.data = self.algo.data
+
+    def run(self, speed='mid'):
+        clrscr()
+        runner = self.algo.sort()
+        self.data = runner.__next__()
+        winwidth_old = os.get_terminal_size()[0]
+        sorted_arr = False
+        while not sorted_arr:
+            if self.data['sorted'] is True:
+                sorted_arr = True
+
+            winwidth_new = os.get_terminal_size()[0]
+            comparing_idx = self.data['comparing_data'].values()
+            swapping_idx = self.data['swapping_data'].values()
+            pass_number = self.data['pass']
+            fixed = self.data.get('fixed', len(self.algo.arr))
+            visual_fixed = pass_number-1
+            comparisons = self.data['comparisons']
+            swaps = self.data['swaps']
+            winwidth = winwidth_new
+            if sorted_arr:
+                comparing_idx = []
+                swapping_idx = []
+            if winwidth_new != winwidth_old:
+                clrscr()
+
+            label = f'''
+{Style.BRIGHT}
+{("Total Passes : "+ str(pass_number)).center(winwidth//2)}{
+("Fixed Elements : " + str(visual_fixed)).center(winwidth//2)}
+{("Comparisons : " + str(comparisons)).center(winwidth//2)}{
+("Swaps : " + str(swaps)).center(winwidth//2)}
+{Style.BRIGHT}{
+Fore.GREEN if self.data["comparing"] else Fore.RED
+}{"COMPARING".center(winwidth)}{
+Style.RESET_ALL}
+{Style.BRIGHT}{
+Fore.LIGHTGREEN_EX if self.data['swapping'] else Fore.LIGHTRED_EX
+}{"SWAPPING".center(winwidth)}{
+Style.RESET_ALL}
+
+{"Array : " + ("[" + " ".join([
+    (Style.DIM + str(val) + Style.RESET_ALL)
+    if (idx not in comparing_idx) and
+    (idx not in swapping_idx) and (idx < fixed
+    )
+    else (Fore.RED + Style.BRIGHT + str(val) + Style.RESET_ALL)
+    if self.data['comparing'] and (idx in comparing_idx)
+    else (Fore.GREEN + Style.BRIGHT + str(val) + Style.RESET_ALL)
+    if self.data['swapping'] and (idx in swapping_idx)
+    else (Style.BRIGHT + Fore.LIGHTCYAN_EX + str(val) + Style.RESET_ALL)
+    if idx > fixed
+    else (Style.DIM + str(val) + Style.RESET_ALL)
+    for idx,val in enumerate(self.data['arr'])
+    ])+"]" + Style.RESET_ALL)}
+
+{Style.BRIGHT + Fore.LIGHTRED_EX
++ self.data['msg'].center(winwidth) + Style.RESET_ALL}
+'''
+            print(POS + self.mainlabel + label)
+            time.sleep(1*self.speeds[speed])
+            winwidth_old = winwidth_new
+            try:
+                self.data = runner.__next__()
+            except StopIteration:
+                pass
+        deinit()
+
+
